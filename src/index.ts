0ce7f2b3e4176941c427147125ceb68f1040a185
Niroj Thapa <ngtc0925@gmail.com>
2023-12-21 11:56:01 +0545

    obects

--- src/index.ts
+++ src/index.ts
@@ -14,3 +14,14 @@ enum Size {Small=1,Medium, Large};
 let mySize: Size=Size.Medium;
 ///return numerc value
 console.log("your size",mySize);
+
+
+//function 
+
+function calculateTax(income:number,taxYear=2022):number {
+//    let x; //unsused value is true
+    if(taxYear <2022)
+    return income*1.1;
+
+    return income *1.2
+}
